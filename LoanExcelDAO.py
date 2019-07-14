7c61054649b9d76c15443b2b26de2d27031a4753
Farmerjoe12 <afarmelo@yahoo.com>
2019-07-14 16:16:33 -0400

    Change the LoanExcelDAO to merely represent the accessor of data

--- LoanExcelDAO.py
+++ LoanExcelDAO.py
@@ -1,58 +1,41 @@
 import xlrd
 import re
 from Loan import Loan
+from RepaymentSchedule import RepaymentSchedule
 
 class LoanExcelDAO:
-    
-    def __init__(self, path, term=15):
-        self.path = path
-        self.loans = self._get_loan_list(term)
+    """ A Data Access Object which gets Loan information
+        from persistant storage in the form of an Excel spreadsheet.
+
+        Args:
+        path: The absolute path to the spreadsheet on your system
+    """
+    def __init__(self, path):
+        self._path = path
+        self._loans = self._get_loan_list()
 
 
-    def sort_by_interest_rate(self, order="asc"):
-        if order == "desc":
-            result = sorted(self.loans, key=lambda loan:loan.interest_rate, reverse=True)
-        else:
-            result = sorted(self.loans, key=lambda loan: loan.interest_rate)
-        return result
-
-
-    def get_total_mthly_payment(self):
-        result = 0
-        for loan in self.loans:
-            pmt = loan.monthly_payment
-            result += pmt
-
-        return result
-
-
-    def get_total_principal(self):
-        result = 0
-        for loan in self.loans:
-            result += loan.principal
-        return result
-
-
-    def _get_loan_list(self, term):
-        book = xlrd.open_workbook(self.path)
+    def _get_loan_list(self):
+        """ Open a workbook, parse data, and extract loan info """
+        book = xlrd.open_workbook(self._path)
         loan_sheet = book.sheet_by_index(0)
         name_col = 0
         principal_col = 1
         interest_rate_col = 2
-        term_col = 3
 
         name_reg = re.compile("^Loan [0-9]*$")
-        loans = []
+        result = []
         for row in range(loan_sheet.nrows):
             name = loan_sheet.cell(row, name_col).value
             princ = loan_sheet.cell(row, principal_col).value
             int_rate = loan_sheet.cell(row, interest_rate_col).value
-            term_val = loan_sheet.cell(row, term_col).value
             
             if name_reg.match(name):
-                if int(term_val) > term:
-                    term_val = term
-                temp = Loan(name, int_rate, princ, term_val)
-                loans.append(temp)
+                temp = Loan(name, int_rate, princ)
+                result.append(temp)
         
-        return loans
+        return result
+
+
+    def get_loans(self):
+        return self._loans
--- LoanExcelDAO_test.py
+++ LoanExcelDAO_test.py
@@ -7,40 +7,8 @@ class LoanExcelDAO_test(unittest.TestCase):
     def test_loan_list_size(self):
         path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
         target = LoanExcelDAO(path)
-        loans = target.loans
+        loans = target.get_loans()
         self.assertEqual(len(loans), 8)
-
-
-    def test_get_total_mthly_payment(self):
-        path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
-        target = LoanExcelDAO(path)
-        self.assertAlmostEqual(target.get_total_mthly_payment(), 460.84, delta=.50)
-
-
-    def test_sort_by_interest_rate_asc(self):
-        path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
-        target = LoanExcelDAO(path)
-        loan_list = target.sort_by_interest_rate()
-        fixture = [0.0429, 0.0429, 0.0466, 0.0466, 0.04865, 0.0505, 0.0505, 0.06365]
-        for i in range(len(fixture)):
-            self.assertEqual(loan_list[i].interest_rate, fixture[i])
-
-
-    def test_sort_by_interest_rate_desc(self):
-        path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
-        target = LoanExcelDAO(path)
-        loan_list = target.sort_by_interest_rate(order="desc")
-        fixture = [0.0429, 0.0429, 0.0466, 0.0466, 0.04865, 0.0505, 0.0505, 0.06365]
-        fixture.sort(reverse = True)
-        for i in range(len(fixture)):
-            self.assertEqual(loan_list[i].interest_rate, fixture[i])
-
-
-    def test_get_total_principal(self):
-        path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
-        target = LoanExcelDAO(path)
-        self.assertAlmostEqual(target.get_total_principal(), 57565.23, delta=0.5)
-
         
 if __name__ == "__main__":
     unittest.main()
