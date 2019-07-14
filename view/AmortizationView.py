3977a4a9deb3b45e920246f930c8bbbaed44fb34
Farmerjoe12 <afarmelo@yahoo.com>
2019-07-14 16:50:30 -0400

    Refactor Amort View and remove deprecated methods relating to lists of loans

--- view/AmortizationView.py
+++ view/AmortizationView.py
@@ -1,46 +1,23 @@
-from Loan import Loan
+from model.Loan import Loan
+from model.RepaymentSchedule import RepaymentSchedule
 import matplotlib.pyplot as plt
 
 class AmortizationView:
+    """ A Repayment Schedule can be viewed convieniently in a graph format.
 
-
-    def plot_single_amortization(self, loan):
-        """ View the amortization chart of a single loan.
-
-            Args:
-            loan: The loan to view
-        """
-        amort_list = loan.get_amortization(loan.principal)
-
-        plt.title(loan.name)
-        self._plot_loan(amort_list)
-        plt.show()
+        The graph shows the amounts paid to interest and principal over time,
+        as well as the principal remaining throughout the term.
+    """
 
     
-    def plot_list_amortization(self, loan_list):
-        """ View the amortization chart of a list of loans.
+    def plot_single_payment_sched(self, schedule, title):
+        """ View the payment schedule of a single loan in graph format.
 
             Args:
-            loan_list: The list of loans to view
+            schedule: The payment schedule of a loan
         """
-        amort_list = []
-
-        for loan in loan_list:
-            amort_list.append(loan.get_amortization_list(loan.principal))
-
-        plt.title("Amortization of multiple loans")
-        self._plot_all_loans(amort_list)
-        plt.show()
-
-
-    def plot_comparison(self, amort_list1, amort_list2, term, principal):
-        plt.title("Loan comparison")
-
-        self._plot_loan(amort_list1)
-        self._plot_loan(amort_list2)
-            
-        self._format_plot(term+10, principal+1000)
-
+        plt.title(title)
+        self._plot_loan(schedule)
         plt.show()
 
 
@@ -65,13 +42,15 @@ class AmortizationView:
         plt.legend()
 
     
-    def _plot_loan(self, amort_list):
-        """ Convert an amortization list into a plottable format. An amortization
-            consists of a variety of data points, over time the data compounds
+    def _plot_loan(self, schedule):
+        """ Convert a payment schedule into a plottable format. 
+        
+            An amortization consists of a variety of data points, 
+            when graphed over time the data compounds
             to show a trend which can be visualized in a graph.
 
             Args:
-            amort_list: An amortization
+            schedule: A payment schedule
         """
         total_interest_paid = []
         total_principal_paid = []
@@ -80,48 +59,15 @@ class AmortizationView:
 
         total_interest = 0
         total_principal = 0
-        for i in range (len(amort_list)):
-            total_interest += amort_list[i][0]
+        for i in range (len(schedule)):
+            total_interest += schedule[i][0]
             total_interest_paid.append(total_interest)
 
-            total_principal += amort_list[i][1]
+            total_principal += schedule[i][1]
             total_principal_paid.append(total_principal)
 
-            balance_remaining.append(amort_list[i][2])
+            balance_remaining.append(schedule[i][2])
 
             term.append(i)
         
         self._plot(total_interest_paid, total_principal_paid, balance_remaining, term)
-
-
-    def _plot_all_loans(self, amort_list):
-        total_interest_paid = []
-        total_principal_paid = []
-        balance_remaining = []
-        term = []
-
-        total_interest = 0
-        total_principal = 0
-
-        for i in range(len(amort_list[0])):
-            term.append(i)
-            total_balance = 0
-
-            for amort in amort_list:
-                total_interest += amort[i][0]
-                total_principal += amort[i][1]
-                total_balance += amort[i][2]
-
-            total_interest_paid.append(total_interest)
-            total_principal_paid.append(total_principal)
-            balance_remaining.append(total_balance)
-    
-        self._plot(total_interest_paid, total_principal_paid, balance_remaining, term)
-
-
-if __name__ == "__main__":
-    loan_0666 = Loan("Loan 0666", 6.66, 15000, 15)
-    loan_06662 = Loan("Loan 06662", 6.66, 15000, 10)
-
-    view = AmortizationView()
-    view.plot_single_amortization(loan_0666)
