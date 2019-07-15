e24daf873a43838755b296eef9ace9782f8daac5
Adam <AFarmelo@yahoo.com>
2019-07-15 11:00:36 -0400

    Remove hard coded path from main

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def _print_int_rates(loan_list):
         print("Loan #{} - {}".format(counter, loan.interest_rate))
         counter+=1
 
-
-path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
+        
 target = Loan("Loan 0666", interest_rate=6.66, principal=15000)
 target2 = Loan("Loan 0555", interest_rate=6.66, principal=15000)
 
