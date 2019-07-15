d147f85110322ba51b9ae18c82070d1acb83b230
Farmelo <AF069488@cerner.net>
2019-07-15 13:16:34 -0400

    #3 properly implement setup method and use class variables throughout

--- test/LoanCollection_test.py
+++ test/LoanCollection_test.py
@@ -1,37 +1,38 @@
 import unittest
-from.context import model as model
+from .context import model as model
 from model.LoanCollection import LoanCollection
+from model.Loan import Loan
 
 class LoanCollection_test(unittest.TestCase):
     
     
-    def setup(self):
-        l1 = Loan("Loan 1", 2.45, 15000)
-        l2 = Loan("Loan 2", 3.24, 15000)
-        l3 = Loan("Loan 3", 1.34, 15000)
-        l4 = Loan("Loan 4", 0.24, 15000)
+    def setUp(self):
+        self.l1 = Loan("Loan 1", 2.45, 15000)
+        self.l2 = Loan("Loan 2", 3.24, 15000)
+        self.l3 = Loan("Loan 3", 1.34, 15000)
+        self.l4 = Loan("Loan 4", 0.24, 15000)
         
-        loans = [l1, l2, l3, l4]
-        target = LoanCollection(loans)
+        self.loans = [l1, l2, l3, l4]
+        self.target = LoanCollection(loans)
         
         
     def test_to_string(self):
         fixtureString = "4 loans in collection\n"
         fixtureString += "========\n"
-        fixtureString += l1.toString() + "\n"
+        fixtureString += self.l1.toString() + "\n"
         fixtureString += "========\n"
-        fixtureString += l2.toString() + "\n"
+        fixtureString += self.l2.toString() + "\n"
         fixtureString += "========\n"
-        fixtureString += l3.toString() + "\n"
+        fixtureString += self.l3.toString() + "\n"
         fixtureString += "========\n"
-        fixtureString += l4.toString() + "\n"
+        fixtureString += self.l4.toString() + "\n"
         fixtureString += "========\n"
         
-        assertEquals(fixtureString, target.to_string())
+        self.assertEquals(fixtureString, target.to_string())
         
     
     def test_sort_by_interest_aesc(self):        
-        target.sort_by_interest()
+        self.target.sort_by_interest()
         
         self.assertEqual(l4, target[0])
         self.assertEqual(l3, target[1])
@@ -40,7 +41,7 @@ class LoanCollection_test(unittest.TestCase):
         
     
     def test_sort_by_interest_desc(self):        
-        target.sort_by_interest(desc)
+        self.target.sort_by_interest("desc")
         
         self.assertEqual(l2, target[0])
         self.assertEqual(l1, target[1])
@@ -72,7 +73,7 @@ class LoanCollection_test(unittest.TestCase):
         
         loans = [l1, l2, l3, l4]
         target = LoanCollection(loans)
-        target.sort_by_principal()
+        target.sort_by_principal("desc")
         
         self.assertEqual(l4, target[0])
         self.assertEqual(l1, target[1])
@@ -80,5 +81,5 @@ class LoanCollection_test(unittest.TestCase):
         self.assertEqual(l2, target[3])
         
     
-if "__name__" == "__main__":
+if __name__ == '__main__':
     unittest.main()
