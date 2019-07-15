b1939566dca804e8d61bb2d8f1c9f17b50941f1b
Adam <AFarmelo@yahoo.com>
2019-07-15 11:48:35 -0400

    #3 Change sort order to string type

--- model/LoanCollection.py
+++ model/LoanCollection.py
@@ -18,19 +18,19 @@ class LoanCollection:
             print("========")
 
             
-    def sort_by_interest_rate(self, order=aesc):
-        if order==desc:
+    def sort_by_interest_rate(self, order="aesc"):
+        if order=="desc":
             sorted(self._loan_list, key=_get_interest_rate_key, reverse=True)
-        elif order==aesc:
+        elif order=="aesc":
             sorted(self._loan_list, key=_get_interest_rate_key)
         else:
             pass    
     
     
-    def sort_by_principal(self, order=aesc):
-        if order==desc:
+    def sort_by_principal(self, order="aesc"):
+        if order=="desc":
             sorted(self._loan_list, key=_get_principal_key, reverse=True)
-        elif order==aesc:
+        elif order=="aesc":
             sorted(self._loan_list, key=_get_principal_key)
         else:
             pass    
