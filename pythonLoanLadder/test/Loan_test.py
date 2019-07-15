afad7af75bdcf9163ec8dbdc359070538130b1c9
Farmelo <AF069488@cerner.net>
2019-07-15 14:25:08 -0400

    Add expected % and change to full percent (not / 100)

--- pythonLoanLadder/test/Loan_test.py
+++ pythonLoanLadder/test/Loan_test.py
@@ -7,7 +7,7 @@ class Loan_test(unittest.TestCase):
     def test_to_string(self):
         target = Loan("Loan 0666", 6.66, 15000)
         expected = "Loan Name: {}\nPrincipal: {}\n".format("Loan 0666", 15000)
-        expected += "Interest Rate: {}".format(0.0666)
+        expected += "Interest Rate: %{}".format(6.66)
         self.assertEqual(expected, target.to_string())
 
             
