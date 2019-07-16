68c5c7f087858d31ba8478ecc351fe82c6706286
Farmerjoe12 <afarmelo@yahoo.com>
2019-07-16 09:21:03 -0400

    Skip ExcelDAO testing if the path given doesn't exist

--- pythonLoanLadder/test/LoanExcelDAO_test.py
+++ pythonLoanLadder/test/LoanExcelDAO_test.py
@@ -1,11 +1,14 @@
 import unittest
+from pathlib import Path
 from model.LoanExcelDAO import LoanExcelDAO
 
+path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
+
+@unittest.skipUnless(Path(path).is_file() == True, "Test file doesn't exist")
 class LoanExcelDAO_test(unittest.TestCase):
 
 
     def test_loan_list_size(self):
-        path = "C:\\Users\\Adam\\OneDrive - Arizona State University\\~School\\Finances.xlsx"
         target = LoanExcelDAO(path)
         loans = target.get_loans()
         self.assertEqual(len(loans), 8)
