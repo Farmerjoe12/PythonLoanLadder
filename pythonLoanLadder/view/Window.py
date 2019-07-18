5c580643bfe09c67b960b16c12126e0afba1170c
Farmerjoe12 <afarmelo@yahoo.com>
2019-07-18 13:51:22 -0400

    #6 Implement a basic GUI with a handler

---
+++ pythonLoanLadder/view/Window.py
@@ -0,0 +1,39 @@
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = "Window"
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.statusBar().showMessage('Message in statusbar.')
+
+        button = QPushButton('Button', self)
+        button.setToolTip('This is a button')
+        button.move(100,70)
+        button.clicked.connect(self.on_click)
+        
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        print("Button clicked")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
