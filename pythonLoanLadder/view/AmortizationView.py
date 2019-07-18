5c2a3d5729d00d52cc22f809707a6e9cc0edfcdc
Farmerjoe12 <afarmelo@yahoo.com>
2019-07-18 14:48:59 -0400

    #6 Refactor plotting methods to make view be displayed as a child of a window

--- pythonLoanLadder/view/AmortizationView.py
+++ pythonLoanLadder/view/AmortizationView.py
@@ -1,5 +1,11 @@
 from model.Loan import Loan
 from model.RepaymentSchedule import RepaymentSchedule
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 """ A Repayment Schedule can be viewed convieniently in a graph format.
@@ -7,89 +13,117 @@ import matplotlib.pyplot as plt
     The graph shows the amounts paid to interest and principal over time,
     as well as the principal remaining throughout the term.
 """
-    
-def plot_single_payment_sched(schedule, title):
-    """ View the payment schedule of a single loan in graph format.
+class AmortizationView(FigureCanvas):
 
-        Args:
-        schedule: The payment schedule of a loan
-    """
-    plt.title(title)
-    _plot_loan(schedule)
-    plt.grid(True)
-    plt.show()
+    def __init__(self, payment_schedule, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        #self.axes = fig.add_subplot(111)
 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
 
-def plot_two_payment_sched(sched1, title1, sched2, title2):
-    """ Plot two payment schedules side by side:
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
 
-        Args:
-        sched1: Loan 1's payment schedule
-        sched2: Loan 2's payment schedule
-        title: Overall plot title
-    """
-    plt.figure(figsize=(10,6))
+        self._sched = payment_schedule
+        self.plot("Title")
 
-    plt.subplot(121)
-    _plot_loan(sched1)
-    plt.grid(True)
-    plt.title(title1)
-
-    plt.subplot(122)
-    _plot_loan(sched2)
-    plt.grid(True)
-    plt.title(title2)
-
-    plt.show()
+        
+    def plot(self, title):
+        plt.title(title)
+        self._plot_loan(self._sched)
+        plt.grid(True)
+        self.draw()
 
 
-def _format_plot(term_length, max_principal):
-    plt.axis([0,term_length, 0,max_principal])
-    plt.grid(b=True, which='both', axis='both')
-    plt.xlabel("Months")
-    plt.ylabel("Dollars")
+    def plot_on_window(self, title):
+        plt.title(title)
+        self._plot_loan(self._sched)
+        plt.grid(True)
+        self.draw()
 
 
-def _plot(interest, principal, balance, term):
-    int_line, = plt.plot(term, interest, 'b', linewidth=1.0)
-    int_line.set_label('Total Interest Paid')
-    plt.legend()
+    def plot_single_payment_sched(self, schedule, title):
+        """ View the payment schedule of a single loan in graph format.
 
-    princ_line, = plt.plot(term, principal, 'g', linewidth=1.0)
-    princ_line.set_label('Total Principal Paid')
-    plt.legend()
-
-    bal_line, = plt.plot(term, balance, 'r', linewidth=1.0)
-    bal_line.set_label('Total Balance')
-    plt.legend()
+            Args:
+            schedule: The payment schedule of a loan
+        """
+        plt.title(title)
+        self._plot_loan(schedule)
+        plt.grid(True)
+        plt.show()
 
 
-def _plot_loan(schedule):
-    """ Convert a payment schedule into a plottable format. 
-    
-        An amortization consists of a variety of data points, 
-        when graphed over time the data compounds
-        to show a trend which can be visualized in a graph.
+    def plot_two_payment_sched(self, sched1, title1, sched2, title2):
+        """ Plot two payment schedules side by side:
 
-        Args:
-        schedule: A payment schedule
-    """
-    total_interest_paid = []
-    total_principal_paid = []
-    balance_remaining = []
-    term = []
+            Args:
+            sched1: Loan 1's payment schedule
+            sched2: Loan 2's payment schedule
+            title: Overall plot title
+        """
+        plt.figure(figsize=(10,6))
 
-    total_interest = 0
-    total_principal = 0
-    for i in range (len(schedule)):
-        total_interest += schedule[i][0]
-        total_interest_paid.append(total_interest)
+        plt.subplot(121)
+        self._plot_loan(sched1)
+        plt.grid(True)
+        plt.title(title1)
 
-        total_principal += schedule[i][1]
-        total_principal_paid.append(total_principal)
+        plt.subplot(122)
+        self._plot_loan(sched2)
+        plt.grid(True)
+        plt.title(title2)
 
-        balance_remaining.append(schedule[i][2])
+        plt.show()
 
-        term.append(i)
-    
-    _plot(total_interest_paid, total_principal_paid, balance_remaining, term)
+
+    def _format_plot(self, term_length, max_principal):
+        plt.axis([0,term_length, 0,max_principal])
+        plt.grid(b=True, which='both', axis='both')
+        plt.xlabel("Months")
+        plt.ylabel("Dollars")
+
+
+    def _plot(self, interest, principal, balance, term):
+        ax = self.figure.add_subplot(111)
+        ax.plot(term, interest, 'b', linewidth=1.0, label = "interest")
+
+        ax.plot(term, principal, 'g', linewidth=1.0, label = "principal")
+
+        ax.plot(term, balance, 'r', linewidth=1.0, label = "balance")
+        ax.grid(True)
+        ax.legend(("interest", "principal", "balance"), loc='upper right')
+
+
+    def _plot_loan(self, schedule):
+        """ Convert a payment schedule into a plottable format. 
+        
+            An amortization consists of a variety of data points, 
+            when graphed over time the data compounds
+            to show a trend which can be visualized in a graph.
+
+            Args:
+            schedule: A payment schedule
+        """
+        total_interest_paid = []
+        total_principal_paid = []
+        balance_remaining = []
+        term = []
+
+        total_interest = 0
+        total_principal = 0
+        for i in range (len(schedule)):
+            total_interest += schedule[i][0]
+            total_interest_paid.append(total_interest)
+
+            total_principal += schedule[i][1]
+            total_principal_paid.append(total_principal)
+
+            balance_remaining.append(schedule[i][2])
+
+            term.append(i)
+        
+        self._plot(total_interest_paid, total_principal_paid, balance_remaining, term)
